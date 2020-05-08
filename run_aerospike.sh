338fa12f69cc76edd1315378749d9f4f693fc8a7
Michael Bonfils <bonfils.michael@protonmail.com>
2020-05-08 23:02:56 +0200

    Fix wording

--- run_aerospike.sh
+++ run_aerospike.sh
@@ -2,5 +2,5 @@
 
 docker run -d --rm --name aerospike-dev aerospike
 
-MONGO_SERVER="$(docker inspect aerospike-dev -f '{{.NetworkSettings.Networks.bridge.IPAddress}}')"
-echo "export MONGO_SERVER=${MONGO_SERVER}"
+AEROSPIKE_SERVER="$(docker inspect aerospike-dev -f '{{.NetworkSettings.Networks.bridge.IPAddress}}')"
+echo "export AEROSPIKE_SERVER=${AEROSPIKE_SERVER}"
